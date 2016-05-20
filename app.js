c78d3ba69956c9fdd430b7d6c506fa61eb9e87d6
Steve Milburn <scmilburn@gmail.com>
2016-05-20 13:57:19 -0400

    spawn mysqldump and gzip backup files

--- app.js
+++ app.js
@@ -3,31 +3,71 @@
 require('dotenv').config();
 
 var Promise = require('bluebird');
+var spawn = require('child_process').spawn;
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS
 });
-var spawn = require('child_process').spawnSync;
+var fs = require('fs');
+
+
 var allDatabases = [];
+var actions = [];
+var ignore = [
+  'information_schema',
+  'book-images'
+];
 
 connection.connect();
 
 Promise.promisifyAll(connection);
 
-
 connection.queryAsync('show databases')
 	.then(dbs => {
+
     dbs.forEach(db => {
-      if (db.Database !== 'information_schema') {
+      if (db.Database !== 'information_schema' && db.Database !== 'book-images') {
         allDatabases.push(db.Database);
-        let ls = spawn('ls', ['-lh', '/usr']);
-        console.log(ls.stdout);
+
+        actions.push(  
+          new Promise(function(resolve, reject) {
+            let wstream = fs.createWriteStream(db.Database + '.sql');            
+            let mysqldump = spawn('mysqldump', [          
+              '-u', 
+              process.env.DB_USER,
+              '-p' + process.env.DB_PASS,
+              db.Database
+            ]);
+
+            mysqldump.stdout
+              .pipe(wstream);
+
+            wstream.on('finish', () => resolve());
+            wstream.on('error', (err) => reject(err));
+          })
+        );
       }
     });
 	})
-  .then(() => console.log(allDatabases))
-	.then(() => process.exit());
+  .then(() => {
+    return Promise.all(actions)
+  })
+  .then(() => {
+    var files = allDatabases.map((db) => db += '.sql');
+
+    return new Promise(function(resolve, reject) {
+      let gzip = spawn('gzip', files);
+
+      gzip.on('close', () => resolve());
+      gzip.stderr.on('data', (data) => reject(data));
+    });
+  })
+	.then(() => process.exit())
+  .catch(err => {
+    console.log(err);
+    process.exit();
+  });
 
 
