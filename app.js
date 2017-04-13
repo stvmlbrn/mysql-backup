bafa4cccf20aca56826833c42297a538b1ce5172
Steve Milburn <scmilburn@gmail.com>
2017-04-13 10:47:35 -0400

    refactor code

--- app.js
+++ app.js
@@ -1,61 +1,38 @@
-var appRoot = require('app-root-path');
-require('dotenv').config({path: appRoot + '/.env'});
+const appRoot = require('app-root-path');
+require('dotenv').config({path: `${appRoot}/.env`});
 
-var Promise = require('bluebird');
-var spawn = require('child_process').spawn;
-var path = require('path');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS
-});
-var fs = require('fs');
-var s3 = require('s3');
-var s3Client = s3.createClient({
-  s3Options: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY
-  }
-});
-var rp = require('request-promise');
-var os = require('os');
-var util = require('util');
+const spawn = require('child_process').spawn;
+const path = require('path');
+const fs = require('fs');
+const rp = require('request-promise');
+const os = require('os');
+const util = require('util');
+const moment = require('moment');
+const Promise = require('bluebird');
 
-var moment = require('moment');
-var backupDate = moment().format('M-D-YY');
+const ignore = require(`${appRoot}/config/ignore`);
+const db = require(`${appRoot}/config/db`);
+const s3Client = require(`${appRoot}/config/s3`);
 
+const monitor = process.argv[2] === '-nomonitor' ? false : true;
 var allDatabases = [];
-var actions = [];
-
-//Add any databases that do not need backed up in the 'ignore' array.
-var ignore = [
-  'information_schema'
-];
 
 //For sending results to CronAlarm
-var formData = {
+const formData = {
   success: 1,
   server: os.hostname(),
   path: __filename,
   message: ''
 };
 
-connection.connect();
-
-Promise.promisifyAll(connection);
-
-rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`)
-  .then(() => {
-    return connection.queryAsync('show databases');
-  })
-	.then(dbs => {
+(monitor ? rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`) : Promise.resolve())
+  .then(() => db.queryAsync('show databases'))
+  .then(dbs => {
+    let actions = [];
     dbs.map(db => {
       if (ignore.indexOf(db.Database) === -1) {
         allDatabases.push(db.Database);
 
-        //Create an array of promises that will spawn the mysqldump child process
-        //and create the individual backup files.
         actions.push(
           new Promise(function(resolve, reject) {
             let wstream = fs.createWriteStream(process.env.BACKUP_PATH + db.Database + '.sql');
@@ -66,8 +43,7 @@ rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`)
               db.Database
             ]);
 
-            mysqldump.stdout
-              .pipe(wstream);
+            mysqldump.stdout.pipe(wstream);
 
             wstream.on('finish', () => resolve());
             wstream.on('error', (err) => reject(err));
@@ -76,12 +52,11 @@ rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`)
       }
     });
 
-    return Promise.all(actions); //execute the backups
-	})
+    return Promise.all(actions);
+  })
   .then(() => {
-    //gzip each backup file.
-    var files = allDatabases.map((db) => process.env.BACKUP_PATH + db + '.sql');
-
+    //gzip each backup file
+    let files = allDatabases.map((db) => process.env.BACKUP_PATH + db + '.sql');
     files.unshift('-f');
 
     return new Promise(function(resolve, reject) {
@@ -92,17 +67,18 @@ rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`)
     });
   })
   .then(() => {
-    actions = [];
+    let actions = [];
+    const backupDate = moment().format('M-D-YY');
 
     //create an array of promises to upload each backup file to S3.
     allDatabases.map(db => {
       actions.push(
         new Promise(function(resolve, reject) {
           var params = {
-            localFile: process.env.BACKUP_PATH + db +'.sql.gz',
+            localFile: `${process.env.BACKUP_PATH}${db}.sql.gz`,
             s3Params: {
               Bucket: process.env.S3_BUCKET,
-              Key: backupDate + '/' + db + '.sql.gz',
+              Key: `${backupDate}/${db}.sql.gz`,
               ServerSideEncryption: 'AES256',
               StorageClass: 'STANDARD_IA' //STANDARD_IA = infrequent access = cheaper storage costs.
             }
@@ -117,15 +93,19 @@ rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`)
 
      return Promise.all(actions); //perform the uploads
   })
-  .catch(err => {
+  .catch((err) => {
     formData.success = 0;
     formData.message = util.inspect(err);
   })
   .finally(() => {
-    return rp({
-      method: 'POST',
-      uri: `http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/end`,
-      form: formData
-    })
+    if (monitor) {
+      return rp({
+        method: 'POST',
+        uri: `http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/end`,
+        form: formData
+      })
+    }
+
+    return Promise.resolve();
   })
   .then(() => process.exit());
