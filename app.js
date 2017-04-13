086e01c959e2f72f07af95641b04d958ca3cff69
Steve Milburn <scmilburn@gmail.com>
2017-04-13 11:07:20 -0400

    added comments

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const ignore = require(`${appRoot}/config/ignore`);
 const db = require(`${appRoot}/config/db`);
 const s3Client = require(`${appRoot}/config/s3`);
 
-const monitor = process.argv[2] === '-nomonitor' ? false : true;
-var allDatabases = [];
+const monitor = process.argv[2] === '-nomonitor' ? false : true;  //determine if using CronAlarm
+var allDatabases = []; //names of all databases to be backed up
 
 //For sending results to CronAlarm
 const formData = {
@@ -25,11 +25,13 @@ const formData = {
   message: ''
 };
 
+// If using CronAlarm, call the start API
+// Otherwise, start the promise chain with Promise.resolve()
 (monitor ? rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`) : Promise.resolve())
   .then(() => db.queryAsync('show databases'))
-  .then(dbs => {
+  .then((dbs) => {
     let actions = [];
-    dbs.map(db => {
+    dbs.map((db) => {
       if (ignore.indexOf(db.Database) === -1) {
         allDatabases.push(db.Database);
 
@@ -71,7 +73,7 @@ const formData = {
     const backupDate = moment().format('M-D-YY');
 
     //create an array of promises to upload each backup file to S3.
-    allDatabases.map(db => {
+    allDatabases.map((db) => {
       actions.push(
         new Promise(function(resolve, reject) {
           var params = {
