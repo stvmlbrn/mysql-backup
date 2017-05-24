43ffa88f3f059bf3f843e9ba57833cf03e751a2c
Steve Milburn <scmilburn@gmail.com>
2017-05-24 19:37:35 -0400

    Use zlib instead of spawning gzip

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const os = require('os');
 const util = require('util');
 const moment = require('moment');
 const Promise = require('bluebird');
+const zlib = require('zlib')
 
 const ignore = require(`${appRoot}/config/ignore`);
 const db = require(`${appRoot}/config/db`);
 const s3Client = require(`${appRoot}/config/s3`);
 
-const monitor = process.argv[2] === '-nomonitor' ? false : true;  //determine if using CronAlarm
-var allDatabases = []; //names of all databases to be backed up
+const monitor = process.argv[2] === '-nomonitor' ? false : true;  // determine if using CronAlarm
+const allDatabases = []; // will hold the name of each databases to be backed up
 
 //For sending results to CronAlarm
 const formData = {
@@ -30,70 +31,58 @@ const formData = {
 (monitor ? rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`) : Promise.resolve())
   .then(() => db.queryAsync('show databases'))
   .then((dbs) => {
-    let actions = [];
-    dbs.map((db) => {
-      if (ignore.indexOf(db.Database) === -1) {
-        allDatabases.push(db.Database);
+    /*
+      Create an array of promises that will create a compressed
+      backup file for each database
+    */
+    let promises = dbs.map((db) => {
+      if (ignore.indexOf(db.Database) === -1) { // make sure database is not in the ignore list
+        allDatabases.push(db.Database); // to be used later
+        const gzip = zlib.createGzip();
+        const wstream = fs.createWriteStream(process.env.BACKUP_PATH + db.Database + '.sql.gz');
+        const mysqldump = spawn('mysqldump', [
+          '-h',
+          process.env.DB_HOST,
+          '-u',
+          process.env.DB_USER,
+          '-p' + process.env.DB_PASS,
+          db.Database
+        ]);
 
-        actions.push(
-          new Promise(function(resolve, reject) {
-            let wstream = fs.createWriteStream(process.env.BACKUP_PATH + db.Database + '.sql');
-            let mysqldump = spawn('mysqldump', [
-              '-u',
-              process.env.DB_USER,
-              '-p' + process.env.DB_PASS,
-              db.Database
-            ]);
+        return new Promise(function(resolve, reject) {
+          mysqldump.stdout.on('error', reject).pipe(gzip).pipe(wstream);
 
-            mysqldump.stdout.pipe(wstream);
-
-            wstream.on('finish', () => resolve());
-            wstream.on('error', (err) => reject(err));
-          })
-        );
+          wstream.on('finish', resolve);
+          wstream.on('error', reject);
+        });
       }
     });
 
-    return Promise.all(actions);
+    return Promise.all(promises);
   })
   .then(() => {
-    //gzip each backup file
-    let files = allDatabases.map((db) => process.env.BACKUP_PATH + db + '.sql');
-    files.unshift('-f');
-
-    return new Promise(function(resolve, reject) {
-      let gzip = spawn('gzip', files);
-
-      gzip.on('close', () => resolve());
-      gzip.stderr.on('data', (data) => reject(data));
-    });
-  })
-  .then(() => {
-    let actions = [];
     const backupDate = moment().format('M-D-YY');
 
-    //create an array of promises to upload each backup file to S3.
-    allDatabases.map((db) => {
-      actions.push(
-        new Promise(function(resolve, reject) {
-          let params = {
-            localFile: `${process.env.BACKUP_PATH}${db}.sql.gz`,
-            s3Params: {
-              Bucket: process.env.S3_BUCKET,
-              Key: `${backupDate}/${db}.sql.gz`,
-              ServerSideEncryption: 'AES256',
-              StorageClass: 'STANDARD_IA' //STANDARD_IA = infrequent access = cheaper storage costs.
-            }
-          };
-          let uploader = s3Client.uploadFile(params);
+    // create an array of promises to upload each backup file to S3.
+    let promises = allDatabases.map((db) => {
+      return new Promise(function(resolve, reject) {
+        const params = {
+          localFile: `${process.env.BACKUP_PATH}${db}.sql.gz`,
+          s3Params: {
+            Bucket: process.env.S3_BUCKET,
+            Key: `${backupDate}/${db}.sql.gz`,
+            ServerSideEncryption: 'AES256',
+            StorageClass: 'STANDARD_IA' //STANDARD_IA = infrequent access = cheaper storage costs.
+          }
+        };
+        const uploader = s3Client.uploadFile(params);
 
-          uploader.on('end', () => resolve());
-          uploader.on('error', (err) => reject(err));
-        })
-      );
+        uploader.on('end', resolve);
+        uploader.on('error', reject);
+      });
     });
 
-     return Promise.all(actions); //perform the uploads
+     return Promise.all(promises);
   })
   .catch((err) => {
     formData.success = 0;
